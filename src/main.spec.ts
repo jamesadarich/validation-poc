82455a3af5a421fffc968c48419673b83d1a4ac7
James Richford <jamesrichford@googlemail.com>
2017-07-09 23:00:43 +0100

    add some tests

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,31 @@
 import { Expect, Test, TestFixture } from "alsatian";
 
-@TestFixture("example test fixture")
-export class ExampleTestFixture {
+@TestFixture("main export")
+export class MainExportTests {
 
     @Test("doesn't throw an error")
     public noError() {
         Expect(() => require("./main")).not.toThrow();
+    }
+
+    @Test("conatins all expected keys")
+    public allExpectedKeys() {
+        const expectedKeys = [
+            "isValid",
+            "messagesFor",
+            "Required",
+            "IValidationDecorator",
+            "createDecorator",
+            "MinLength",
+            "MatchesProperty",
+            "MatchesRegex",
+            "EmailAddress"
+        ];
+
+        const mainExportKeys = Object.keys(require("./main"));
+
+        expectedKeys.forEach(key => Expect(mainExportKeys).toContain(key));
+
+        Expect(mainExportKeys.length).toBe(expectedKeys.length);
     }
 }
